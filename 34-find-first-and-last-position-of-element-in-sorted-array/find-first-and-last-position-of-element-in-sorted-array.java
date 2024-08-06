c39b21a5af36a83fe2ecca3f906eacd87766b9e4
abi-2003 <138606787+abi-2003@users.noreply.github.com>
2024-08-06 22:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (45.57%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,51 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] result = {-1, -1};
+
+        // Find the first occurrence of target
+        result[0] = findStartingIndex(nums, target);
         
-        // Edge case for empty array
-        if (nums.length == 0) {
-            return result;
-        }
-        
+        // Find the last occurrence of target
+        result[1] = findEndingIndex(nums, target);
+
+        return result;
+    }
+
+    private int findStartingIndex(int[] nums, int target) {
+        int index = -1;
         int start = 0;
         int end = nums.length - 1;
-        
+
         while (start <= end) {
             int mid = (start + end) / 2;
-            
-            if (nums[mid] == target) {
-                // Find the left boundary
-                int left = mid;
-                while (left >= 0 && nums[left] == target) {
-                    left--;
-                }
-                result[0] = left + 1;
-
-                // Find the right boundary
-                int right = mid;
-                while (right < nums.length && nums[right] == target) {
-                    right++;
-                }
-                result[1] = right - 1;
-                
-                return result;
-            } else if (nums[mid] > target) {
+            if (nums[mid] >= target) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
+            if (nums[mid] == target) {
+                index = mid;
+            }
         }
-        
-        return result;
+        return index;
+    }
+
+    private int findEndingIndex(int[] nums, int target) {
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+        return index;
     }
 }
