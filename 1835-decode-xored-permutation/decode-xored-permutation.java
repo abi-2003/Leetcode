e81173c747055034e25a2e21da02706fc7d8a753
abi-2003 <138606787+abi-2003@users.noreply.github.com>
2024-08-16 20:01:10 +0530

    Time: 2 ms (100.00%) | Memory: 64.9 MB (6.43%) - LeetSync

--- 1835-decode-xored-permutation/decode-xored-permutation.java
+++ 1835-decode-xored-permutation/decode-xored-permutation.java
@@ -1,19 +1,23 @@
 class Solution {
     public int[] decode(int[] encoded) {
-        int n = encoded.length + 1;
-        int a = 0, b = 0;
-        for (int i = 0; i < n - 1; i += 2) {
-            a ^= encoded[i];
+        
+        int n=encoded.length+1;
+             int a=0,b=0;
+        for( int i=1;i<encoded.length;i+=2){
+             a^=encoded[i];
         }
-        for (int i = 1; i <= n; ++i) {
-            b ^= i;
+        for(int i=1;i<=n;i++){
+             b^=i;
         }
-        int[] per = new int[n];
-        per[n - 1] = a ^ b;
-        for (int i = n - 2; i >= 0; i--) {
-            per[i] = encoded[i] ^ per[i + 1];
+
+        int first=a^b;
+
+        int []per=new int[n];
+        per[0]=first;
+        
+        for( int i=0;i<encoded.length;i++){
+            per[i+1]=encoded[i]^per[i];
         }
         return per;
-        
     }
 }
