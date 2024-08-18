42693dcb8479d38086bc4a00e44a4c354cf3a18e
abi-2003 <138606787+abi-2003@users.noreply.github.com>
2024-08-18 16:26:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (6.28%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,17 @@
-class Solution {
-    public int fib(int n) {
-        int []dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        
-        if(n<=1){
-            return n;
-        }
-        if(dp[n] != -1){
-            return n;
-        }
-        dp[n]=fib(n-1)+fib(n-2);
-        return dp[n];
-        
+class Solution 
+{
+    public int fib(int N)
+    {
+        if(N <= 1)
+            return N;
+
+		int[] fib_cache = new int[N + 1];
+		fib_cache[1] = 1;
+
+		for(int i = 2; i <= N; i++)
+		{
+			fib_cache[i] = fib_cache[i - 1] + fib_cache[i - 2];
+		}
+		return fib_cache[N];
     }
 }
