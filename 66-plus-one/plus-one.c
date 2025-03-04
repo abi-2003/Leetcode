14a6007dbdc3be4f01a13f553737248acf4c67cb
abi-2003 <138606787+abi-2003@users.noreply.github.com>
2025-03-04 17:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (52.22%) - LeetSync

--- 66-plus-one/plus-one.c
+++ 66-plus-one/plus-one.c
@@ -1,23 +1,20 @@
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
     *returnSize = digitsSize;
 
-    if (digits[digitsSize - 1] < 9) {
-        digits[digitsSize - 1]++;
+   
+
+    //int carry = 1;
+    for (int i = digitsSize - 1; i >= 0; i--) {
+         if (digits[i] < 9) {
+        digits[i]++;
         return digits;
     }
-
-    int carry = 1;
-    for (int i = digitsSize - 1; i >= 0; i--) {
-        digits[i] += carry;
-        carry = digits[i] / 10;
-        digits[i] %= 10;
-
-        if (carry == 0) return digits;
+      digits[i]=0;
     }
 
     digits = realloc(digits, sizeof(int) * (digitsSize + 1));
     for (int i = digitsSize; i > 0; i--) {
-        digits[i] = digits[i - 1];
+        digits[i] = 0;
     }
     digits[0] = 1;
 
