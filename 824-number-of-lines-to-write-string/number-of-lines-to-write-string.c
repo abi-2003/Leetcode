e543c0b33ff252d3f57377e537073ef0e9aabdbd
abi-2003 <138606787+abi-2003@users.noreply.github.com>
2025-03-04 13:35:58 +0530

    Time: 3 ms (49.41%) | Memory: 7.8 MB (57.18%) - LeetSync

--- 824-number-of-lines-to-write-string/number-of-lines-to-write-string.c
+++ 824-number-of-lines-to-write-string/number-of-lines-to-write-string.c
@@ -4,24 +4,23 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* numberOfLines(int* widths, int widthsSize, char * s, int* returnSize){
+      
+        int *res=(int*)malloc(2*sizeof(int));
+      *returnSize=2;
+        int line=1, width=0;
 
-int* result = (int*)malloc(2 * sizeof(int));
-    *returnSize = 2;
-
-    int lines = 1, width = 0;
-
-    for (int i = 0; s[i] != '\0'; i++) {
-        int w = widths[s[i] - 'a'];  
-
-        if (width + w > 100) {  
-            lines++;
-            width = w;  
-        } else {
-            width += w;  
+        for(int i=0;i<strlen(s);i++){
+            int w=widths[s[i]-'a'];
+            if(width+w>100){
+                line++;
+                width=w;
+            }
+            else{
+                width+=w;
+            }
+            
         }
-    }
-
-    result[0] = lines;
-    result[1] = width;
-    return result;
+        res[0]=line;
+        res[1]=width;
+        return res;
 }
